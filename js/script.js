4a25c07092986087844858de8f3f45369772a8f9
LucasKenji01 <rm94233@fiap.com.br>
2022-05-18 21:19:51 -0300

    Base do exercicio

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//Math é uma classe que no javascript possui métodos
+//que retornam valores matemáticos
+
+//POW()
+//alert(Math.pow(2, 3))
+
+//RANDOM - Retorna números randômicos entre 0 e 1
+//alert(Math.random())
+
+//ROUND - Arredonda pra cima e pra baixo
+//alert(Math.round(Math.random()))
+
+//FLOOR - Arredondar pra baixo
+//alert(Math.floor(Math.random()))
+
+//CEIL - Arredondar para cima
+//alert(Math.ceil(Math.random()))
+
+//var nomesAlunos = ['Eduarda', 'Luan', 'Vitor', 'Rodrigo', 'Samuel', 'Marcelo']
+
+//alert(Math.round(Math.random() * nomesAlunos.length))
+
+//Valores aleatórios
+
+//setTimeOut(alert, 1000)
+setInterval(trocaCores,100)
+
+function trocaCores(){
+
+    const divsFilhas = document.querySelectorAll(".box")
+
+    for(const elemento of divsFilhas) {
+
+        var red = Math.round(Math.random() * 255)
+        var green = Math.round(Math.random() * 255)
+        var blue = Math.round(Math.random() * 255)
+
+        //CONCATENAÇÃO
+        //elemento.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"
+
+        //INTERPOLAÇÃO
+        elemento.style.backgroundColor = `rgb(${red} ,${green} ,${blue})`
+
+    }
+}
